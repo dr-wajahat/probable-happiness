3c98cffdf60475ff723cb094918736ee047f03ca
Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2020-10-10 13:43:10 +0500

    Modified the Stereo

--- Beta.cpp
+++ Beta.cpp
@@ -2,11 +2,29 @@
 
 using namespace std;
 
+void printBar()
+{
+	for (int i=0;i<9;i++)
+	cout<<"*";
+	
+	cout<<endl;
+	for (int i=0;i<9;i++)
+{	
+	if (i>2 && i<7)
+	cout<<"*";
+	else
+	cout<<"-";
+}
+cout<<endl;
+}
+
 int main ()
 {
-	cout<<"Hello There"<<endl;	
+	cout<<"Hello There"<<endl;
+	printBar();
 	for (int i=0;i<5;i++)
 		cout<<"Pak Turk Friendship Long live."<<endl;	
+	printBar();
 
 	bool doYouAgree;
 	
@@ -14,18 +32,14 @@ int main ()
 	cout<<"Enter y for yes and n for no: ";
 	cin>>option;
 	
-	if(opt=='y')
+	if(option=='y')
 	doYouAgree=true;
 	else
 	doYouAgree=false;
 	
+	cout<<endl;
 	if(doYouAgree)
-	cout<<"Bhai Nahana Parey gaa";
+	cout<<"Khsuh Giyaal'din Ottomon Empire";
 	else
-	cout<<"Khaire hi hia Cheetey";
-	
-	if(isTodayFriday)
-	cout<<"Bhai Nahana Parey gaa";
-	else
-	cout<<"Khaire hi hia Cheetey";
+	cout<<"Tu mera Put Chut'tey kr";
 }
